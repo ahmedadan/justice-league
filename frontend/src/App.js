c45fd3f3dc5e41891326f7a25195e39747695e8c
Jessie Wu <jessie.wu223@gmail.com>
2021-09-24 14:33:23 -0400

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 import WelcomePage from './pages/welcome.js'
+import IndividualProfile from './pages/individualprofile.js'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <WelcomePage/>
+//       <IndividualProfile />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+const showIndividualProfile = () => {
+  if (window.location.pathname === "/") {
+    return <WelcomePage/>;
+  }
+};
+
+const showWelcomePage = () => {
+  if (window.location.pathname === "/individualprofile") {
+    return <IndividualProfile />;
+  }
+};
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <WelcomePage/>
+    <div>
+      {showWelcomePage()}
+      {showIndividualProfile()}
     </div>
   );
-}
-
+};
 export default App;
