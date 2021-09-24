242cd9ecee560507c63129cdfcf5f394cdabd201
Jessie Wu <jessie.wu223@gmail.com>
2021-09-24 00:47:42 -0400

    reformat code and styling changes

---
+++ frontend/src/pages/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Create a Stream!</h3>
+      </div>
+    )
+  }
+}
+
+export default Test;
